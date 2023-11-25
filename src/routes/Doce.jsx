7bfc45e1b8aa0c58fbb9bec365cb7299882ea32d
WallaceB2 <118089332+WallaceB2@users.noreply.github.com>
2023-11-25 16:18:44 -0300

    Atualizei Tela Pizza P/ ficar de acordo com padrão.

--- src/routes/Doce.jsx
+++ src/routes/Doce.jsx
@@ -1,48 +1,62 @@
-export default function Doce () {
-  return(
-    <div className="p-8 bg-gray-200">
-      <div className="text-red-900 font-bold text-2xl pt-0 pl-4 pr-4 pb-4 border-t-4 border-red-900 rounded-xl bg-white">
-        Doces
-      </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-4">
-        <div className="bg-white p-4 shadow-md rounded-md grid grid-cols-3 gap-4 relative">
-          <div className="col-span-2">
-            <h1 className="text-red-900 font-bold text-2xl">Ingredientes:</h1>
-            <ul className="list-disc ml-4">
-              <li>200 gramas de creme de leite</li>
-              <li>100 gramas de coco ralado seco</li>
-              <li>200ml leite de coco</li>
-              <li>Adoçante a gosto</li>
-              <li>Coco ralado a gosto para enrolar</li>
-              <li>Cravo da índia a gosto para finalizar</li>
-            </ul>
+import { Link, Outlet } from "react-router-dom";
+import NavBar from "../componentes/navbar";
+import BackgroundLayout from "../estilos/VariosBackgrounds";
+import Rodape from "../componentes/rodape";
+
+export default function Pizza() {
+  return (
+    <BackgroundLayout backgroundImage={`url('./src/assets/imagens/img-fundo/Home.png')`}> 
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="bg-transparent p-8 rounded-xl">
+          <NavBar />
+          <Outlet />
+          <div className="flex justify-center">  
+            <div className="text-red-900 max-w-3xl w-full font-bold text-3xl pt-4 pb-4 border-t-4 border-red-900 rounded-xl bg-white text-center">
+              Pizza Baiana
+            </div> 
           </div>
-          <div className="col-span-1 flex flex-col items-center justify-center mt-8 relative">
-            <h2 className=" text-red-900 absolute top-0 font-bold text-2xl">Beijinho</h2>
-            <img
-              src="https://cdn.awsli.com.br/600x450/1837/1837006/produto/229963683/trufa-de-chocolate-branco-com-limao--2--5if5ocur1c.png"
-              width="650"
-              height="850"
-              alt="Beijinho"
-              className="mt-10"
-              class="mt-10 border-2 border-solid border-gray-400 rounded-md hover:border-blue-500 transition duration-300 ease-in-out"
-            />
-          </div>
-          <div className="col-span-3">
-            <h1 className="font-bold text-2xl text-red-900 ">Modo de Preparo:</h1>
-            <ol className="list-decimal ml-4">
-              <li>Reúna todos os ingredientes;</li>
-              <li>Em uma panela, adicione o creme de leite, o coco ralado, o leite de coco, misture bem e deixe cozinhar, mexendo sempre, até incorporar;</li>
-              <li>Acrescente o adoçante (a quantidade que desejar), misture, transfira para um prato e leve para geladeira para gelar;</li>
-              <li>Enrole os beijinhos, passe no coco ralado e coloque o cravo por cima;</li>
-            </ol>
+
+          <div className="flex justify-center">
+            <div className="max-w-3xl w-full grid grid-cols-1 sm:grid-cols-1 gap-6 mt-4">
+              <div className="bg-white border-t-4 border-red-900 rounded-xl p-6 shadow-md rounded-md grid grid-cols-3 gap-6 relative h-full">
+                <div className="col-span-2">
+                  <h1 className="text-red-900 font-bold text-2xl">Ingredientes:</h1>
+                  <ul className="list-disc ml-4">
+                    <li>Massa de pizza</li>
+                    <li>1 xícara (chá) de molho de tomate</li>
+                    <li>150 g de linguiça calabresa</li>
+                    <li>200 g de queijo mussarela fatiado</li>
+                    <li>2 ovos cozidos</li>
+                    <li>1 cebola</li>
+                    <li>Azeitona a gosto</li>
+                    <li>Orégano a gosto</li>
+                  </ul>
+                </div>
+                <div className="col-span- flex flex-col items-center justify-center mt-8 relative overflow-hidden">
+                  <img
+                    src="https://pizzariadesucesso.com/wp-content/uploads/2018/05/pizza-baiana-picante-001-620x395.png"
+                    alt="Pizza Baiana"
+                    className="border-2 mt-10 border-solid border-white"
+                  />
+                </div>
+                <div className="col-span-3">
+                  <h1 className="font-bold text-2xl text-red-900">Modo de Preparo:</h1>
+                  <ol className="list-decimal ml-4">
+                  <li>Pique a linguiça em pedaços pequenos.</li>
+                    <li>Fatie os ovos e a cebola.</li>
+                    <li>Passe o molho de tomate pela massa de pizza.</li>
+                    <li>Disponha a calabresa e os ovos.</li>
+                    <li>Coloque as fatias de queijo por cima.</li>
+                    <li>Acrescente a cebola picada, a azeitona e o orégano com quantidades a gosto.</li>
+                    <li>Asse e sirva.</li>
+                  </ol>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="bg-white p-4 shadow-md rounded-md">Doce 2</div>
-        <div className="bg-white p-4 shadow-md rounded-md">Doce 3</div>
-        <div className="bg-white p-4 shadow-md rounded-md">Doce 4</div>
       </div>
-    </div>
-  )
+      <Rodape/>
+    </BackgroundLayout>
+  );
 }
-
