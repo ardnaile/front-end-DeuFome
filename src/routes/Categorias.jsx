d6ae071217a390eb640ec14e58a6e0286b713893
WallaceB2 <118089332+WallaceB2@users.noreply.github.com>
2023-12-02 20:32:22 -0300

    Adicionei imagens a tela de categorias.

--- src/routes/Categorias.jsx
+++ src/routes/Categorias.jsx
@@ -15,73 +15,73 @@ export default function Categorias () {
                 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Pizza-lista" >
-                        <img className='w-96  hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl' src="https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />      
+                        <img className='w-96  hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl' src="src\assets\imagens\categoriaImagens\pizzaa.jpg" alt="" />      
                     </Link>  
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Massas-lista">
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\massa.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\docee.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Pizza-lista" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl' src="https://images.pexels.com/photos/2147491/pexels-photo-2147491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />      
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl' src="src\assets\imagens\categoriaImagens\saladaa.jpg" alt="" />      
                     </Link>  
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Massas-lista">
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\carne.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 h-64 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\sushii.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96  hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\peixe.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\cerveja.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\sopa.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\hamburguer.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\panquecas.jpg" alt="" />
                     </Link>
                 </div>
 
                 <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
                     <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="https://images.pexels.com/photos/19065709/pexels-photo-19065709/free-photo-of-lanchonete-comendo-comida-alimentacao.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\frutosMar.jpg" alt="" />
                     </Link>
                 </div>
        
