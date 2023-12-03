ab85a1a68e0ea81b3a28c31c45bb18ba4e1c2c6c
WallaceB2 <118089332+WallaceB2@users.noreply.github.com>
2023-12-02 22:41:52 -0300

    Adicionado interações no restante da tela.

--- src/routes/Categorias.jsx
+++ src/routes/Categorias.jsx
@@ -39,65 +39,135 @@ export default function Categorias() {
             </Link>
         </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\docee.jpg" alt="" />
-                    </Link>
-                </div>
+        <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+            <Link to="/Doce">
+              <div className='relative w-96'>
+                <img
+                  className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                  src="src\assets\imagens\categoriaImagens\docee.jpg"
+                  alt="Doce"
+                />
+                <p className="text-white text-8xl font-bold text-roboto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Doce</p>
+              </div>
+            </Link>
+          </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Pizza-lista" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl' src="src\assets\imagens\categoriaImagens\saladaa.jpg" alt="" />      
-                    </Link>  
+        <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+            <Link to="/Pizza-lista">
+              <div className='relative w-96'>
+                <img
+                  className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                  src="src\assets\imagens\categoriaImagens\saladaa.jpg"
+                  alt="Salada"
+                />
+                <p className="text-white text-8xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Salada</p>
                 </div>
+            </Link>
+        </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Massas-lista">
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\carne.jpg" alt="" />
-                    </Link>
-                </div>
+          <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+            <Link to="/Massas-lista">
+                <div className='relative w-96'>
+                    <img
+                        className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                        src="src\assets\imagens\categoriaImagens\carne.jpg"
+                        alt="Carne"
+                        />
+                        <p className="text-white text-8xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Carne</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 h-64 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\sushii.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+                <Link to="/Doce">
+                    <div className='relative w-96 h-64'>
+                        <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\sushii.jpg"
+                            alt="Sushi"
+                        />
+                        <p className="text-white text-8xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Sushi</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96  hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\peixe.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+                <Link to="/Doce">
+                    <div className='relative w-96'>
+                        <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\peixe.jpg"
+                            alt="Peixe"
+                        />
+                        <p className="text-white text-8xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Peixe</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\cerveja.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+            <Link to="/Doce">
+                <div className='relative w-96'>
+                    <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\cerveja.jpg"
+                            alt="Cerveja"
+                        />
+                        <p className="text-white text-8xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Cerveja</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\sopa.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+                <Link to="/Doce">
+                    <div className='relative w-96'>
+                        <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\sopa.jpg"
+                            alt="Sopa"
+                        />
+                        <p className="text-white text-8xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Sopa</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\hamburguer.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+                <Link to="/Doce">
+                    <div className='relative w-96'>
+                        <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\hamburguer.jpg"
+                            alt="Hambúrguer"
+                        />
+                        <p className="text-white text-6xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Hambúrguer</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\panquecas.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+                <Link to="/Doce">
+                    <div className='relative w-96'>
+                        <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\panquecas.jpg"
+                            alt="Panquecas"
+                        />
+                        <p className="text-white text-6xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Panqueca</p>
+                    </div>
+                </Link>
+            </div>
 
-                <div className='text-center text-white font-bold text-2xl pb-4 pt-6'> 
-                    <Link to="/Doce" >
-                        <img className='w-96 hover:blur-sm hover transition duration-300 cursor-pointer shadow-3xl rounded-3xl ' src="src\assets\imagens\categoriaImagens\frutosMar.jpg" alt="" />
-                    </Link>
-                </div>
+            <div className='text-center text-white font-bold text-2xl pb-4 pt-6 relative'>
+                <Link to="/Doce">
+                    <div className='relative w-96'>
+                        <img
+                            className='w-full cursor-pointer shadow-3xl rounded-3xl hover:blur-sm hover:shadow-md'
+                            src="src\assets\imagens\categoriaImagens\frutosMar.jpg"
+                            alt="Frutos do Mar"
+                        />
+                        <p className="text-white text-6xl font-bold absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-0 hover:opacity-100 transition duration-300">Frutos do Mar</p>
+                    </div>
+                </Link>
+            </div>
        
             </div>
             <Rodape/>
